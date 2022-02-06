b9ce586bd01c3226e8cd161b5f3311ad5eecf754
Dubenis L <43077233+dubis78@users.noreply.github.com>
2022-02-06 16:16:52 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const morgan = require("morgan");
+const express = require("express");
+const app = express();
+const routes = require("./routes/routes");
+
+// Ajustes
+app.set("port", 4000);
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes//
+app.use("/api", routes);
+
+// Ajustes del servidor
+app.listen(app.get("port"), () => {
+  console.log(`Server running on port ${app.get("port")}`);
+});
