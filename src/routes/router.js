f1cbf45f3d885445bd333cb8dadf60d2c754cfae
dubis78 <dubis85201@hotmail.com>
2022-02-11 02:55:42 -0500

    add login and register endpoints

--- src/routes/router.js
+++ src/routes/router.js
@@ -26,4 +26,38 @@ router.get(`/users/comments`, (req, res) => {
   });
 });
 
+router.post(`/register/`, (req, res) => {
+  const { e_mail, password } = req.body;
+  mysqlConnection.query(
+    `INSERT INTO users (e_mail, password) VALUES ('${e_mail}', '${password}')`,
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ message: `Usuario registrado` });
+      } else {
+        res.json({ message: `Usuario no registrado` });
+      }
+    }
+  );
+});
+
+router.post(`/login/`, (req, res) => {
+  const { e_mail, password } = req.body;
+  mysqlConnection.query(
+    `SELECT * FROM users WHERE e_mail='${e_mail}' AND password='${password}'`,
+    (err, rows, fields) => {
+      if (err) {
+        res.json({ message: `Error` });
+        return console.log(err.message);
+      }
+      if (rows.length > 0) {
+        res.json({ message: `Welcome` });
+      } else {
+        res.json({
+          message: `email or password no not match`
+        });
+      }
+    }
+  );
+});
+
 module.exports = router;
