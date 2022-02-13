6e232116377268007438581384fb59147a4a37be
dubis78 <dubis85201@hotmail.com>
2022-02-13 17:48:34 -0500

    improvement status error messages

--- src/routes/router.js
+++ src/routes/router.js
@@ -17,20 +17,24 @@ router.get(`/public-posts-some-comments`, (req, res) => {
   url.self = urlGenerator(req);
   mysqlConnection.query(query(1, ``), (err, rows, fields) => {
     if (!err) {
-      const filtData = data2json(rows).map((element) => {
-        element.tolt_comments = element.comments.length;
-        element.comments.splice(5);
-        return element;
-      });
-      const serialized = jsonapiFormat(
-        filtData,
-        `posts`,
-        false,
-        null,
-        true,
-        url
-      );
-      res.json(serialized);
+      if (rows.length > 0) {
+        const filtData = data2json(rows).map((element) => {
+          element.tolt_comments = element.comments.length;
+          element.comments.splice(5);
+          return element;
+        });
+        const serialized = jsonapiFormat(
+          filtData,
+          `posts`,
+          false,
+          null,
+          true,
+          url
+        );
+        res.json(serialized);
+      } else {
+        res.status(404).json(`Data Not Found`);
+      }
     } else {
       res.status(500).json(err.code);
     }
@@ -42,140 +46,155 @@ router.get(`/private-posts`, (req, res) => {
   url.self = urlGenerator(req);
   mysqlConnection.query(query(2, ``), (err, rows, fields) => {
     if (!err) {
-      const serialized = jsonapiFormat(rows, `posts`, false, null, null, url);
-      res.json(serialized);
+      if (rows.length > 0) {
+        const serialized = jsonapiFormat(rows, `posts`, false, null, null, url);
+        res.json(serialized);
+      } else {
+        res.status(401).json(`unauthorized`);
+      }
     } else {
       res.status(500).json(err.code);
     }
   });
 });
 
-router.get(`/post`, (req, res) => {
+router.get(`/posts/:id`, (req, res) => {
   let url = {};
   url.self = urlGenerator(req);
-  const { id } = req.query;
+  const { id } = req.params;
   const user_id = 0;
   let queryWhere = ``;
   if (user_id) {
-    queryWhere = `WHERE C.id = ${id} AND C.is_published = 0 AND C.user_id = ${user_id}`;
+    queryWhere = `WHERE P.id = ${id} AND P.is_published = 0 AND P.user_id = ${user_id}`;
   } else {
-    queryWhere = `WHERE C.id = ${id} AND C.is_published = 1`;
+    queryWhere = `WHERE P.id = ${id} AND P.is_published = 1`;
   }
   mysqlConnection.query(query(3, queryWhere), (err, rows, fields) => {
     if (!err) {
-      const serialized = jsonapiFormat(rows, `posts`, false, null, null, url);
-      res.json(serialized);
+      if (rows.length > 0) {
+        const serialized = jsonapiFormat(rows, `posts`, false, null, null, url);
+        res.json(serialized);
+      } else {
+        res.status(401).json(`unauthorized`);
+      }
     } else {
       res.status(500).json(err.code);
     }
   });
 });
 
-router.post(`/post`, (req, res) => {
+router.post(`/posts`, (req, res) => {
   const { title, body, slug, is_published } = req.body;
   const user_id = 0;
   const str = `${user_id}, ${title}, ${body}, ${slug}, ${is_published}`;
   if (user_id) {
     mysqlConnection.query(query(4, str), (err, rows, fields) => {
       if (!err) {
-        res.code(201).json(rows);
+        res.status(201).json(rows);
       } else {
         res.status(500).json(err.code);
       }
     });
   } else {
-    res.code(401).json(`unauthorized`);
+    res.status(401).json(`unauthorized`);
   }
 });
 
-router.put(`/post`, (req, res) => {
+router.put(`/posts/:id`, (req, res) => {
   const { title, body, slug, is_published } = req.body;
   const user_id = 0;
-  const { id } = req.query;
+  const { id } = req.params;
   if (user_id) {
     mysqlConnection.query(
       query(5, ``),
       [title, body, slug, is_published, user_id, id],
       (err, rows, fields) => {
         if (!err) {
-          res.code(204).json(rows);
+          res.status(204).json(rows);
         } else {
           res.status(500).json(err.code);
         }
       }
     );
   } else {
-    res.code(401).json(`unauthorized`);
+    res.status(401).json(`unauthorized`);
   }
 });
 
-router.delete(`/post`, (req, res) => {
+router.delete(`/posts/:id`, (req, res) => {
   const user_id = 0;
-  const { id } = req.query;
+  const { id } = req.params;
   if (user_id) {
     mysqlConnection.query(query(6, ``), [user_id, id], (err, rows, fields) => {
       if (!err) {
-        res.code(204).json(rows);
+        res.status(204).json(rows);
       } else {
         res.status(500).json(err.code);
       }
     });
   } else {
-    res.code(401).json(`unauthorized`);
+    res.status(401).json(`unauthorized`);
   }
 });
 
-router.get(`/comments-per-user`, (req, res) => {
+router.get(`/comments-per-user/:id`, (req, res) => {
   let url = {};
   url.self = urlGenerator(req);
-  const { id } = req.query;
+  const { id } = req.params;
   const user_id = 0;
   let queryWhere = ``;
   id ? (queryWhere = `WHERE U.id = ${id}`) : (queryWhere = ``);
   mysqlConnection.query(query(0, queryWhere), (err, rows, fields) => {
     if (!err) {
-      const serialized = jsonapiFormat(
-        data2json(rows),
-        `users`,
-        true,
-        null,
-        true,
-        url
-      );
-      res.json(serialized);
+      if (rows.length > 0) {
+        const serialized = jsonapiFormat(
+          data2json(rows),
+          `users`,
+          true,
+          null,
+          true,
+          url
+        );
+        res.json(serialized);
+      } else {
+        res.status(404).json(`Data Not Found`);
+      }
     } else {
       res.status(500).json(err.code);
     }
   });
 });
 
-router.get(`/comments-per-post`, (req, res) => {
+router.get(`/comments-per-post/:id`, (req, res) => {
   let url = {};
   url.self = urlGenerator(req);
-  const { id } = req.query;
+  const { id } = req.params;
   const user_id = 0;
   let queryWhere = ``;
   id ? (queryWhere = `WHERE C.id = ${id}`) : (queryWhere = ``);
   console.log(query(1, queryWhere));
   mysqlConnection.query(query(1, queryWhere), (err, rows, fields) => {
     if (!err) {
-      console.log(data2json(rows));
-      const serialized = jsonapiFormat(
-        data2json(rows),
-        `posts`,
-        true,
-        null,
-        true,
-        url
-      );
-      res.json(serialized);
+      if (rows.length > 0) {
+        const serialized = jsonapiFormat(
+          data2json(rows),
+          `posts`,
+          true,
+          null,
+          true,
+          url
+        );
+        res.json(serialized);
+      } else {
+        res.status(404).json(`Data Not Found`);
+      }
     } else {
       res.status(500).json(err.code);
     }
   });
 });
 
-router.post(`/comment`, (req, res) => {
+router.post(`/comments`, (req, res) => {
   const { body, is_published } = req.body;
   const { post_id } = req.query;
   const user_id = 0;
@@ -183,50 +202,50 @@ router.post(`/comment`, (req, res) => {
     const str = `${user_id}, ${post_id}, ${body}, ${is_published}`;
     mysqlConnection.query(query(8, str), (err, rows, fields) => {
       if (!err) {
-        res.code(201).json(rows);
+        res.status(201).json(rows);
       } else {
         res.status(500).json(err.code);
       }
     });
   } else {
-    res.code(401).json(`unauthorized`);
+    res.status(401).json(`unauthorized`);
   }
 });
 
-router.put(`/comment`, (req, res) => {
+router.put(`/comments/:id`, (req, res) => {
   const { body, is_published } = req.body;
   const user_id = 0;
-  const { id } = req.query;
+  const { id } = req.params;
   if (user_id) {
     mysqlConnection.query(
       query(9, ``),
       [body, is_published, user_id, id],
       (err, rows, fields) => {
         if (!err) {
-          res.code(204).json(rows);
+          res.status(204).json(rows);
         } else {
           res.status(500).json(err.code);
         }
       }
     );
   } else {
-    res.code(401).json(`unauthorized`);
+    res.status(401).json(`unauthorized`);
   }
 });
 
-router.delete(`/comment`, (req, res) => {
+router.delete(`/comments/:id`, (req, res) => {
   const user_id = 0;
-  const { id } = req.query;
+  const { id } = req.params;
   if (user_id) {
     mysqlConnection.query(query(10, ``), [user_id, id], (err, rows, fields) => {
       if (!err) {
-        res.code(204).json(rows);
+        res.status(204).json(rows);
       } else {
         res.status(500).json(err.code);
       }
     });
   } else {
-    res.code(401).json(`unauthorized`);
+    res.status(401).json(`unauthorized`);
   }
 });
 
